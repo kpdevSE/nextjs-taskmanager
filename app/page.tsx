3151b7ce33a61f1b01175fd499c7ff025016723f
kpdevSE <kanishkapasindu6@gmail.com>
2024-11-18 11:33:19 +0530

    'ddone'

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-500 font-extrabold font-serif">
+        this is the home page of this
+      </h1>
+    </div>
+  );
+}
